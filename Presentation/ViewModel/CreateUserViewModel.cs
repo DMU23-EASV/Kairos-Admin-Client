21cf924fba7c4d6cac4f924c674a6160ed22ddca
Mads Knudsen <mads@rknudsen.dk>
2024-11-25 20:01:57 +0100

    chore(CreateUserViewModel): Added Documentation to class

--- Presentation/ViewModel/CreateUserViewModel.cs
+++ Presentation/ViewModel/CreateUserViewModel.cs
@@ -3,9 +3,14 @@ using System.Windows.Input;
 using WPF_MVVM_TEMPLATE.Entitys;
 
 namespace WPF_MVVM_TEMPLATE.Presentation.ViewModel;
-
+/*
+ * CreateUserViewModel Contains the form for creation of new users.
+ * We use a dictionary to hold all the textboxes, this is to ensure we dont repeat our selves more than needed.
+ * 
+ */
 public class CreateUserViewModel : ViewModelBase
 {
+    //Our Dictionary with all our Textboxes, initialized to get a valid bind.
     private readonly Dictionary<string, string> _fields = new()
     {
         { "TextBoxFirstName", "" },
@@ -22,6 +27,7 @@ public class CreateUserViewModel : ViewModelBase
         { "TextBoxComment", "" }
     };
     
+    //Our generalized getter and setter for our fields. We validate through our dictionary
     public string this[string field]
     {
         get => _fields.ContainsKey(field) ? _fields[field] : "";
@@ -35,6 +41,7 @@ public class CreateUserViewModel : ViewModelBase
         }
     }
 
+    //Our savePerson method, we check to see if any fields are empty and if they are we save them to keep track of which.
     private void SavePerson(object obj)
     {
         var emptyFields = _fields.Where(field => string.IsNullOrWhiteSpace(field.Value))
@@ -43,17 +50,15 @@ public class CreateUserViewModel : ViewModelBase
 
         if (emptyFields.Any())
         {
-            Console.WriteLine($"The following fields are empty: {string.Join(", ", emptyFields)}");
+            Console.WriteLine($"The following fields are empty :D {string.Join(", ", emptyFields)}");
         }
         else
         {
-            Console.WriteLine("All fields are valid!");
+            Console.WriteLine("All fields are valid! :-)");
             // Perform save logic
         }
     }
-
+    //Our ICommand, required to make connection between View and ViewModel 
     public ICommand SavePersonCommand => new CommandBase(SavePerson);
-    
-    
     
 }
