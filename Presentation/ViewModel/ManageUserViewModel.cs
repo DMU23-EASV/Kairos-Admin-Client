8d3d2864224419108d3eb5628183993a5b5c722a
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-11-28 20:33:59 +0100

    feat(ManageUser): Manageuser
    Added Icommand for loading user

--- Presentation/ViewModel/ManageUserViewModel.cs
+++ Presentation/ViewModel/ManageUserViewModel.cs
@@ -9,25 +9,24 @@ namespace WPF_MVVM_TEMPLATE.Presentation.ViewModel;
 
 public class ManageUserViewModel : ViewModelBase
 {
-    public ObservableCollection<ManageUserDTO> Users {
-        get
-        {
-            return _users;
-        }
+    public ObservableCollection<ManageUserDTO> Users
+    {
+        get { return _users; }
         set
         {
-            _users = value; 
+            _users = value;
             OnPropertyChanged();
         }
     }
+
     private ObservableCollection<ManageUserDTO> _users;
 
     public ManageUserViewModel()
     {
         _users = new ObservableCollection<ManageUserDTO>();
-        LoadUsers(); 
+        LoadUsers();
     }
-    
+
     public async void LoadUsers()
     {
         var ws = new WebService("http://localhost:8080/");
@@ -35,19 +34,22 @@ public class ManageUserViewModel : ViewModelBase
         Console.WriteLine("Loading users...");
         _users.Clear();
         var users = await lu.FetchUsers();
-        
+
         // Validating respons from the API.
-        if (users == null || users.Count <= 0) return; 
-        
+        if (users == null || users.Count <= 0) return;
+
         users.ForEach(user =>
         {
             if (user != null) _users.Add(user);
         });
     }
 
-    public ICommand AddUserCommand = new CommandBase(AddUser);
-    private static void AddUser(object obj)
+    public ICommand LoadUsersCommand => new CommandBase(o =>
     {
-        //ViewModelController.Instance.SetCurrentViewModel<CreateUser>();
-    }
+        LoadUsers();
+    }); 
+    
+    
+
 }
+
