078ffbe9e6fa998dba76c3bb3a2af71f57e03b52
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-12-04 13:22:53 +0100

    feat(ExportTaskData): Created DTO, and started work on VM

---
+++ Entitys/DTOs/SimpleUserDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace WPF_MVVM_TEMPLATE.Entitys.DTOs;
+
+public class SimpleUserDTO
+{
+    public string Username { get; set; }
+}
--- Presentation/ViewModel/ExportTaskViewModel.cs
+++ Presentation/ViewModel/ExportTaskViewModel.cs
@@ -1,6 +1,11 @@
-﻿namespace WPF_MVVM_TEMPLATE.Presentation.ViewModel;
+﻿using System.Collections.ObjectModel;
+using WPF_MVVM_TEMPLATE.Entitys.DTOs;
+
+namespace WPF_MVVM_TEMPLATE.Presentation.ViewModel;
 
 public class ExportTaskViewModel : ViewModelBase
 {
-    
+    public ObservableCollection<SimpleUserDTO> Users { get; set; }
+    public ObservableCollection<SimpleUserDTO> SelectedUsers { get; set; }
+    public bool AnonymizeUsername, AnonymizeEmail;
 }
