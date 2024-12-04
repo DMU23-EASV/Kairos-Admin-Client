4c016ccd17b23dd830851afaecfe0120b6186d1a
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-12-04 08:37:41 +0100

    feat(update task): UpdateTaskUC
    added usecaseIMPL

---
+++ Application/UpdateTask.cs
@@ -0,0 +1,17 @@
+﻿using WPF_MVVM_TEMPLATE.Entitys;
+using WPF_MVVM_TEMPLATE.InterfaceAdapter;
+
+namespace WPF_MVVM_TEMPLATE.Application;
+
+public class UpdateTask
+{
+    
+    private ITaskRepo _taskRepo;
+        
+    public UpdateTask(ITaskRepo taskRepo)
+    {
+        _taskRepo = taskRepo;
+    }
+    
+    public async Task UpdateTaskAsync(TaskModel task, string endpoint) => await _taskRepo.UpdateTask(task, endpoint);
+}
