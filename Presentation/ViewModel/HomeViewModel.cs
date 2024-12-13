01f40d1750d357a1d01dbaaf886c3505a2116ccc
Alexander Gerckens <112266323+Gercken@users.noreply.github.com>
2024-12-13 10:07:55 +0100

    fix(HomeViewModel):
    Deleted chart for now

--- Presentation/ViewModel/HomeViewModel.cs
+++ Presentation/ViewModel/HomeViewModel.cs
@@ -46,7 +46,6 @@ public class HomeViewModel : ViewModelBase
         _taskRepo = new TaskRepoApi(_webService);
         _loadTasks = new LoadTasks(_taskRepo);
         LoadTasks();
-        GetUsersForChartAsync();
     }
 
     private async void LoadTasks()
@@ -65,80 +64,6 @@ public class HomeViewModel : ViewModelBase
     
     
     
-     private readonly UserRepoApi _userRepoApi;
-    private readonly TaskRepoApi _taskRepoApi;
-
-    public ObservableCollection<ISeries> Series { get; set; } = new ObservableCollection<ISeries>();
-    public ObservableCollection<Axis> XAxes { get; set; } = new ObservableCollection<Axis>
-    {
-        new Axis
-        {
-            Labels = new List<string>(),
-            SeparatorsPaint = new SolidColorPaint(SKColors.White)
-        }
-    };
-    public ObservableCollection<Axis> YAxes { get; set; } = new ObservableCollection<Axis>
-    {
-        new Axis { IsVisible = false }
-    };
-
-    public bool IsReading { get; set; } = true;
-
-    public async Task GetUsersForChartAsync()
-    {
-        var loadUsers = new LoadUsers(_userRepoApi);
-        var users = await loadUsers.GetUsers();
-
-        foreach (var user in users)
-        {
-            var loadTasks = new LoadTasks(_taskRepoApi);
-            var tasks = await loadTasks.GetTaskByUsername(user.Username);
-
-            // Add user data to the chart
-            var taskCount = tasks.Count();
-            AddOrUpdateUser(user.Username, taskCount);
-        }
-    }
-
-    private void AddOrUpdateUser(string username, int value)
-    {
-        var barSeries = Series.OfType<ColumnSeries<int>>().FirstOrDefault(s => s.Name == username);
-
-        if (barSeries != null)
-        {
-            // Update the existing bar's value
-            if (barSeries.Values is ObservableCollection<int> values)
-            {
-                values[0] = value;
-            }
-        }
-        else
-        {
-            // Add a new bar for the user
-            Series.Add(new ColumnSeries<int>
-            {
-                Name = username,
-                Values = new ObservableCollection<int> { value },
-                Fill = new SolidColorPaint(SKColors.Blue)
-            });
-
-            // Add username to XAxis labels
-            XAxes[0].Labels.Add(username);
-        }
-    }
-
-    public async Task StartRaceAsync()
-    {
-        await Task.Delay(1000);
-
-        while (IsReading)
-        {
-            await GetUsersForChartAsync();
-
-            // Simulate delay for race chart update
-            await Task.Delay(2000);
-        }
-    }
 
     #endregion
 
